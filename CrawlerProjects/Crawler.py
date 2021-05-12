a6210739ff6d10e536f5e0aae81e3d3e2e64bb53
ARedSky <1021391131@qq.com>
2021-05-12 15:54:57 +0800

    url

--- CrawlerProjects/Crawler.py
+++ CrawlerProjects/Crawler.py
@@ -1,2 +1,18 @@
 import requests
 from lxml import etree
+url='https://www.timeshighereducation.com/world-university-rankings/2021/world-ranking#!/page/0/length/25/sort_by/name/sort_order/asc/cols/stats'
+headers={
+    'user-agent':' Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36'
+}
+
+
+
+
+
+
+
+
+
+
+
+
