2012e4ab082a34296c5fa84968a658b974a9829d
ARedSky <1021391131@qq.com>
2021-05-12 17:19:43 +0800

    genggai

--- CrawlerProjects/Crawler.py
+++ CrawlerProjects/Crawler.py
@@ -1,12 +1,17 @@
 import requests
 from lxml import etree
+# import matplotlib.pyplot as mlt
+# import seaborn as sns
+
+
 url='https://www.timeshighereducation.com/world-university-rankings/2021/world-ranking#!/page/0/length/25/sort_by/name/sort_order/asc/cols/stats'
 
 headers={
-    'user-agent':' Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36'
+    'user-agent':' Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36'
 }
 
-
+response = requests.get(url,headers).text
+print(response)
 
 
 
