4a521826cb8dc6a769bf5a146fa0eb2a3b5da620
zhouzhenhong <zhouzhenhong@siluzan.com>
2021-06-02 15:44:49 +0800

    wu

---
+++ work/working.py
@@ -0,0 +1,22 @@
+import pandas as pd
+# import numpy as np
+# import matplotlib.pyplot as mpl
+# import xlrd
+# import xlwt
+# path = '.\成功易5月份人民币和香港端口日均消耗.xlsx'
+# sheet=['美金端口']
+# df=pd.read_excel('',sheet_name=sheet)
+# df=xlrd.open_workbook(path,sheet)
+
+print('Hello word!!!')
+
+
+
+
+
+
+
+
+
+
+
