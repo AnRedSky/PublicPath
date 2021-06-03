0d3c459698af7c5c94d386240ba0c4d0c257c258
zhouzhenhong <zhouzhenhong@siluzan.com>
2021-06-03 18:00:19 +0800

    ww

--- work/working.py
+++ work/working.py
@@ -1,22 +1,22 @@
 import pandas as pd
-# import numpy as np
-# import matplotlib.pyplot as mpl
-# import xlrd
-# import xlwt
-# path = '.\成功易5月份人民币和香港端口日均消耗.xlsx'
-# sheet=['美金端口']
-# df=pd.read_excel('',sheet_name=sheet)
-# df=xlrd.open_workbook(path,sheet)
+import numpy as np
+import matplotlib.pyplot as mpl
+import xlrd
+import xlwt
 
-print('Hello word!!!')
+path = r'./成功易5月份人民币和香港端口日均消耗.xlsx'
+# path = "*./2021-4月份谷歌消费活跃账号.csv"
+sheet=['美金端口']
+# df=pd.read_excel(path,sheet_name=sheet)
+df=xlrd.open_workbook(path,sheet)
 
+# df=pd.read_csv(path)
 
+# path = "./2021-4月份谷歌消费活跃账号.csv"
+# pd.read_csv(path)
 
+pd.read_excel(path,sheet)
 
-
-
-
-
-
+import re
 
 
